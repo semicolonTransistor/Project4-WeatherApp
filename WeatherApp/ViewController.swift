e3bc23052354d3c7e999c6ecde6375f1ca28c65e
Jinyu Liu <jliu@sch.org>
2016-05-25 09:34:23 -0400

    Finished SwiftyJSON integration.

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -10,9 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var forecastLabel:UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let manager = AFHTTPSessionManager()
+        manager.GET("http://api.openweathermap.org/data/2.5/forecast/daily?q=London&mode=json&units=metric&cnt=1&appid=5cf1874ae4af126924e1840b6283ec3a",
+            parameters: nil,
+            progress: nil,
+            success: { (operation: NSURLSessionDataTask,responseObject: AnyObject?) in
+                if let responseObject = responseObject {
+                    print("Response: " + responseObject.description)
+                    let json = JSON(responseObject)
+                    if let forecast = json["list"][0]["weather"][0]["description"].string {
+                        self.forecastLabel.text = forecast
+                    }
+                }
+            },
+            failure: { (operation: NSURLSessionDataTask?,error: NSError) in
+                print("Error: " + error.localizedDescription)
+        })
     }
 
     override func didReceiveMemoryWarning() {
---
+++ bridge.h
@@ -0,0 +1,14 @@
+//
+//  bridge.h
+//  WeatherApp
+//
+//  Created by Jinyu Liu on 5/25/16.
+//  Copyright © 2016 Electricfish Electroincs. All rights reserved.
+//
+
+#ifndef bridge_h
+#define bridge_h
+
+#import "WeatherApp/AFNetworking/AFNetworking.h"
+
+#endif /* bridge_h */
